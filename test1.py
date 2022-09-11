6dadfb87bd8a885153305a64520bbb7ec1dc4b91
spvin <113282999+spvin@users.noreply.github.com>
2022-09-11 21:48:36 +0530

    test added

--- test1.py
+++ test1.py
@@ -1,13 +1,13 @@
 import os
 
-def prin_user():
+def prin_user_test():
     print("User name is uday")
 
-def count():
+def count_test():
     for i in range(10):
         print("Number is "+str(i))
 
 if __name__ == "__main__":
-    prin_user()
-    count()
+    prin_user_test()
+    count_test()
     print("Test Ended\n")
