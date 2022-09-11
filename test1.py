47c44b42009b28ef8f3d168a3dfe4ab4912ebae7
spvin <113282999+spvin@users.noreply.github.com>
2022-09-11 21:45:27 +0530

    demo test1

---
+++ test1.py
@@ -0,0 +1,13 @@
+import os
+
+def prin_user():
+    print("User name is uday")
+
+def count():
+    for i in range(10):
+        print("Number is "+str(i))
+
+if __name__ == "__main__":
+    prin_user()
+    count()
+    print("Test Ended\n")
